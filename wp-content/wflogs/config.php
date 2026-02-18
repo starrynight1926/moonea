a514ea8b5892c71d8235713c764c426590b67ad2
starrynight <starrynight1926@gmail.com>
2026-02-18 17:38:54 +0700

    wp-moonea

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1771523352;s:7:"authKey";s:64:"*a0g^CE1DaZPYvJ34dR)SJsTDfC3Zdq8mKt$^USAZFu9XnI{1WV^(A{PyF0mC$d5";s:7:"version";s:5:"1.1.0";}
